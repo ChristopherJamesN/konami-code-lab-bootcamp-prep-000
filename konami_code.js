9147d052076dac1346ed2747d1c5a3c1e570fca0
Chris Nady <nady.christopher@gmail.com>
2017-08-26 20:21:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
 
-  document.body.addEventListener('keydown', function(e))
+  document.body.addEventListener('keydown', function(e));
     // Keep track of index outside of the event handler.
   let index = 0;
 
