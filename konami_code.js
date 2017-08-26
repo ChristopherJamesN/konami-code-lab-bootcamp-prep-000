7927384a5b5feae68e204ade3eff16d65438f7b9
Chris Nady <nady.christopher@gmail.com>
2017-08-26 19:50:43 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
 
-  window.addEventListener('keydown', function(e)) {
+  window.addEventListener('keydown', function(e))
     // Keep track of index outside of the event handler.
   let index = 0;
 
@@ -22,6 +22,4 @@ function init() {
       index = 0;
     }
   }
-}
-
 }
